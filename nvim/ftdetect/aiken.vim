a67b4480dc9875a6095c4d542398ceba1c6fb930
Pizza Wizard <pizzawizard@MacBook-Pro-von-Pizza.local>
2023-11-21 02:11:04 +0200

    lazy and kickstart

--- nvim/ftdetect/aiken.vim
+++
@@ -1,7 +0,0 @@
-" Aiken syntax file auto-detection
-" Language: Aiken
-" Maintainer: KtorZ <matthias.benkort@gmail.com>
-" Copyright: KtorZ <matthias.benkort@gmail.com>
-" License: MPL-2.0
-
-au BufNewFile,BufRead *.ak setf aiken
--- nvim/lua/custom/options.lua
+++ nvim/lua/custom/options.lua
@@ -1,6 +1,3 @@
-vim.opt.relativenumber = true
-vim.opt.number = true
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
@@ -8,9 +5,12 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.autoindent = true
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.opt.titlestring = "%t"
 vim.opt.encoding = "utf-8"
-vim.opt.linebreak = false
+vim.opt.linebreak = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 20
 vim.opt.ruler = true
@@ -23,6 +23,7 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 --vim.opt.cursorline = true
+vim.o.clipboard = 'unnamedplus' -- Yank to clipboard
 vim.opt.colorcolumn = "80"
 vim.wo.signcolumn = "yes"
 vim.opt.swapfile = false
@@ -33,8 +34,18 @@ vim.opt.pumheight = 12
 vim.diagnostic.config({
     signs = true,
     underline = true,
-	float = { border = false },
-	virtual_text = true,
+    float = { border = false },
+    virtual_text = true,
 })
 
--- Setting background transparent
+-- Make line numbers default
+vim.wo.number = true
+-- Enable break indent
+vim.o.breakindent = true
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
